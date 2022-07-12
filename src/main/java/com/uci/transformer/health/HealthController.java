82e1d9d4245a05fe8cc044c38f673d390cf0f15c
Surabhi <surabhi.mahawar@aurigait.com>
2022-07-12 14:41:58 +0530

    api response changes

--- src/main/java/com/uci/transformer/health/HealthController.java
+++ src/main/java/com/uci/transformer/health/HealthController.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.uci.utils.UtilHealthService;
 
+import com.uci.utils.model.ApiResponse;
+import com.uci.utils.model.ApiResponseParams;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -15,6 +17,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,15 +31,8 @@ public class HealthController {
 	private UtilHealthService healthService;
 	
     @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> statusCheck() throws JsonProcessingException, IOException {
+    public ResponseEntity<ApiResponse> statusCheck() throws JsonProcessingException, IOException {
         ObjectMapper mapper = new ObjectMapper();
-        /* Current Date Time */
-        LocalDateTime localNow = LocalDateTime.now();
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        String dateString = fmt.format(localNow).toString();
-        
-        JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.health\",\"ver\":\"3.0\",\"ts\":\"2021-06-26T22:47:05Z+05:30\",\"params\":{\"resmsgid\":\"859fee0c-94d6-4a0d-b786-2025d763b78a\",\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"checks\":[],\"healthy\":false}}");
-        
         JsonNode resultNode = mapper.readTree("{\"checks\":[{\"name\":\"kafka\",\"healthy\":false},{\"name\":\"campaign\",\"healthy\":false}],\"healthy\":true}");
         
         /* Kafka health info */
@@ -60,10 +56,14 @@ public class HealthController {
         } else {
         	((ObjectNode) resultNode).put("healthy", false);
         }
-        
-        ((ObjectNode) jsonNode).put("ts", dateString);
-        ((ObjectNode) jsonNode).put("result", resultNode);
-        
-        return ResponseEntity.ok(jsonNode);
+
+        ApiResponse response = ApiResponse.builder()
+                    .id("api.health")
+                    .params(ApiResponseParams.builder().build())
+                    .responseCode(HttpStatus.OK.name())
+                    .result(resultNode)
+                    .build();
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/uci/transformer/health/ServiceStatusController.java
+++ src/main/java/com/uci/transformer/health/ServiceStatusController.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.uci.utils.UtilHealthService;
+import com.uci.utils.model.ApiResponse;
+import com.uci.utils.model.ApiResponseParams;
 import com.uci.utils.telemetry.LogTelemetryMessage;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,32 +31,27 @@ public class ServiceStatusController {
 	@Autowired
 	private UtilHealthService healthService;
 
-	@RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> kafkaStatusCheck() throws IOException, JsonProcessingException {
-    	JsonNode jsonNode = getResponseJsonNode();
-    	((ObjectNode) jsonNode).put("result", healthService.getKafkaHealthNode());
-        
-        return ResponseEntity.ok(jsonNode);
+    @RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
+    public ResponseEntity<ApiResponse> kafkaStatusCheck() throws IOException, JsonProcessingException {
+        ApiResponse response = ApiResponse.builder()
+                .id("api.service.health.kafka")
+                .params(ApiResponseParams.builder().build())
+                .responseCode(HttpStatus.OK.name())
+                .result(healthService.getKafkaHealthNode())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
-    
+
     @RequestMapping(value = "/health/campaign", method = RequestMethod.GET, produces = { "application/json", "text/json" })
-    public ResponseEntity<JsonNode> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
-    	JsonNode jsonNode = getResponseJsonNode();
-        ((ObjectNode) jsonNode).put("result", healthService.getCampaignUrlHealthNode());
-        
-        return ResponseEntity.ok(jsonNode);
-    }
-    
-    /**
-     * Returns json node for service response
-     * 
-     * @return JsonNode
-     * @throws JsonMappingException
-     * @throws JsonProcessingException
-     */
-    private JsonNode getResponseJsonNode() throws JsonMappingException, JsonProcessingException {
-    	ObjectMapper mapper = new ObjectMapper();
-    	JsonNode jsonNode = mapper.readTree("{\"id\":\"api.content.service.health\",\"ver\":\"3.0\",\"ts\":null,\"params\":{\"resmsgid\":null,\"msgid\":null,\"err\":null,\"status\":\"successful\",\"errmsg\":null},\"responseCode\":\"OK\",\"result\":{\"healthy\":false}}");
-        return jsonNode;
+    public ResponseEntity<ApiResponse> campaignUrlStatusCheck() throws JsonProcessingException, IOException {
+        ApiResponse response = ApiResponse.builder()
+                .id("api.service.health.campaign")
+                .params(ApiResponseParams.builder().build())
+                .responseCode(HttpStatus.OK.name())
+                .result(healthService.getCampaignUrlHealthNode())
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 }
