6fcfeddd5516a697c2f8dd19d1f2ba2d8853aba3
Surabhi <surabhi.mahawar@aurigait.com>
2022-08-08 12:00:16 +0530

    service - health, dev migration

--- src/main/java/com/uci/transformer/health/ServiceStatusController.java
+++ src/main/java/com/uci/transformer/health/ServiceStatusController.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.uci.utils.UtilHealthService;
+import com.uci.utils.model.ApiResponse;
+import com.uci.utils.model.ApiResponseParams;
 import com.uci.utils.telemetry.LogTelemetryMessage;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +30,26 @@ public class ServiceStatusController {
 	
 	@Autowired
 	private UtilHealthService healthService;
+
+    /**
+     * In use by sunbird team - to check service liveliness & readliness
+     * @return
+     * @throws JsonProcessingException
+     */
+    @RequestMapping(value = "/health", method = RequestMethod.GET, produces = { "application/json", "text/json" })
+    public ResponseEntity<ApiResponse> statusCheck() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode resultNode = mapper.readTree("{\"healthy\":true}");
+
+        ApiResponse response = ApiResponse.builder()
+                .id("api.service.health.cassandra")
+                .params(ApiResponseParams.builder().build())
+                .responseCode(HttpStatus.OK.name())
+                .result(resultNode)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
 
 	@RequestMapping(value = "/health/kafka", method = RequestMethod.GET, produces = { "application/json", "text/json" })
     public ResponseEntity<JsonNode> kafkaStatusCheck() throws IOException, JsonProcessingException {
--- src/main/resources/db/migration/006_create_question_assessment.sql
+++ src/main/resources/db/migration/006_create_question_assessment.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE table IF NOT EXISTS question
+CREATE table question
 (
     id              uuid DEFAULT uuid_generate_v4 (),
     form_id         VARCHAR(100) NOT NULL,
@@ -11,13 +11,13 @@ CREATE table IF NOT EXISTS question
     created         TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated         TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
-    );
+);
 
-CREATE INDEX IF NOT EXISTS idx_form ON question(form_id, form_version);
-CREATE INDEX IF NOT EXISTS idx_xPath ON question(x_path);
-CREATE INDEX IF NOT EXISTS idx_ques_type ON question(question_type);
+CREATE INDEX idx_form ON question(form_id, form_version);
+CREATE INDEX idx_xPath ON question(x_path);
+CREATE INDEX idx_ques_type ON question(question_type);
 
-CREATE table IF NOT EXISTS assessment
+CREATE table assessment
 (
     id              uuid DEFAULT uuid_generate_v4 (),
     question        uuid,
@@ -30,10 +30,10 @@ CREATE table IF NOT EXISTS assessment
     updated         TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
     CONSTRAINT fk_question
-    FOREIGN KEY (question)
-    REFERENCES question(id)
-    );
+        FOREIGN KEY (question)
+            REFERENCES question(id)
+);
 
-CREATE INDEX IF NOT EXISTS idx_assessment_bot_id ON assessment(bot_id);
-CREATE INDEX IF NOT EXISTS idx_assessment_user_id ON assessment(user_id);
-CREATE INDEX IF NOT EXISTS idx_assessment_device_id ON assessment(device_id);
+CREATE INDEX idx_assessment_bot_id ON assessment(bot_id);
+CREATE INDEX idx_assessment_user_id ON assessment(user_id);
+CREATE INDEX idx_assessment_device_id ON assessment(device_id);
